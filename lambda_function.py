e9c6b17f6c5c337b3da35bd77407450cbd401754
Amal-V <amalv@qburst.com>
2017-05-05 12:53:07 +0530

    Adding git binary

--- lambda_function.py
+++ lambda_function.py
@@ -18,21 +18,26 @@ release_tag="1.0.0"
 release_branch="master"
 release_name="Last"
 body=""
-github_token=""
-github_org=""
-github_repo=""
-
+github_token="aa082e8fd641c449abf835cea3ba89bea3a2df8f"
+github_org="microservices-today"
+github_repo="ngp-node-server"
+os.environ['PATH']=os.environ['PATH']+":/var/task/git-binary/"
+print os.environ['PATH']
 def callShell(cmd):
+    print cmd
     pipe=subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     (output,error)=pipe.communicate()
+    print output.replace('\n','')
     return output.replace('\n','')
-
+callShell("ls -la /var/task/git-binary/")
+callShell("which git")
+callShell("git --version")
 def lambda_handler( event, context):
     try:
-        repo=git.Repo.clone_from("https://" + github_token + "@github.com/" + github_org + '/' + github_repo +".git", osp.join(os.getcwd(), github_repo), branch="master")
+        repo=git.Repo.clone_from("https://" + github_token + "@github.com/" + github_org + '/' + github_repo +".git", osp.join("/tmp/", github_repo), branch="master")
     except:
         print "Already existing Repo"
-        repo=git.Repo(osp.join(os.getcwd(), github_repo))
+        repo=git.Repo(osp.join("/tmp/", github_repo))
     repo.git.checkout("staging")
     # latest_tag="1.0.0"
     latest_release=urllib2.urlopen("https://api.github.com/repos/"+github_org+
@@ -46,4 +51,3 @@ def lambda_handler( event, context):
         release_note+=commit.message + "\n"
     release_note=release_note.replace('\n','</br>')
     print release_note
-lambda_handler("","")
